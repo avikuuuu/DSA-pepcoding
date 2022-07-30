df42036510add1b4b9e02ba3ea22bb08fd892699
Avdesh kumar <70461207+avikuuuu@users.noreply.github.com>
2022-07-30 10:36:49 +0530

    Update pattern-1.java

--- DSA Level-1/Basics of Programming/Patterns/pattern-1.java
+++ DSA Level-1/Basics of Programming/Patterns/pattern-1.java
@@ -1,23 +1,21 @@
-
-import java.io.*;
 import java.util.*;
 
-public class Main{
+public class Main {
 
-    public static void main(String[] args) throws Exception {
-        // write your code here
-        BufferedReader bufferedReader=new BufferedReader(new InputStreamReader(System.in));
-        int n=Integer.parseInt(bufferedReader.readLine());
-        int[] arr= new int[n];
-        for(int i=0;i<n;i++){
-            arr[i]=Integer.parseInt(bufferedReader.readLine());
-        }
-        for(int i=0;i<arr.length;i++){
-            for(int j=0;j<=i;j++){
-                System.out.print(arr[j]+'\t');
-            }
-            System.out.println();
-        }
+    public static void main(String[] args) {
+       
+
+        // write ur code here
+        Scanner scn = new Scanner(System.in);
+    int n = scn.nextInt();
+
+    for (int i = 1; i <= n; i++)
+    {
+      for (int j = 1; j <= i; j++) //MAGIC LOOP
+      {
+        System.out.print("*	");
+      }
+      System.out.println();
     }
-
+  }
 }
